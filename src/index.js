01c54ce586d1478978df9e2e51eee81108891fea
shehan <s.tharuka1998@gmail.com>
2021-06-20 19:35:20 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from 'serviceWorker';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+/*
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+
+//reportWebVitals();
